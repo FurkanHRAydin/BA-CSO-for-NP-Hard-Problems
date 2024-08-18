08beba303b6dcbe07c30b093708cbddc7e834249
Furkan <furkan.h.aydin@hotmail.de>
2024-08-18 17:06:20 +0200

    Wip

---
+++ Nurse.py
@@ -0,0 +1,18 @@
+class Nurse:
+    def __init__(self, name, num_days, availability, preferences):
+        self.name = name
+        self.schedule = ['None'] * num_days  # Initial leerer Schichtplan für jeden Tag
+        self.availability = availability  # Verfügbare Schichten für jeden Tag
+        self.preferences = preferences  # Bevorzugte Schichten
+
+    def assign_shift(self, day, shift):
+        """
+        Zuweisen einer Schicht an einem bestimmten Tag.
+        """
+        self.schedule[day] = shift
+
+    def get_shift(self, day):
+        """
+        Gibt die zugewiesene Schicht für einen bestimmten Tag zurück.
+        """
+        return self.schedule[day]
